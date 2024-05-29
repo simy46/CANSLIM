7bd310f04635e92d1ca325f9b255912952062ec8
simy46 <missipsa.annane@polymtl.ca>
2024-05-29 13:31:22 -0400

    Update package configurations and lock file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite';
+import copy from 'vite-plugin-static-copy';
 
 export default defineConfig({
+  plugins: [
+    copy({
+      targets: [
+        { src: '/*.html', dest: '' }
+      ]
+    })
+  ],
   server: {
     open: true,
     port: 3000,
