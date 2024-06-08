40a9398d8a3fd57c340ee5db584141f5ce0ea7a6
simy46 <missipsa.annane@polymtl.ca>
2024-06-08 02:29:58 -0400

    env3

--- assets/js/const.js
+++ assets/js/const.js
@@ -1,7 +1,8 @@
-export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL,
-TRENDING_STOCKS_KEY = 'TRENDING_STOCKS',
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL; // Vérifie que la variable est bien définie
+console.log('Server URL:', SERVER_URL); // Ajoute ceci pour vérifier que la variable est bien définie
+
+export const TRENDING_STOCKS_KEY = 'TRENDING_STOCKS',
 TRENDING_STOCKS_TICKERS = 'TRENDING_TICKERS',
 ETAG_KEY = 'stocksEtag',
 ACTIVE_INDEX = 'activeContainerIndex',
 STOCK_SELECTION = 'stockSelection';
- 
